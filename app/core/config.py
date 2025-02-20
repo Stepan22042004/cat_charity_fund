62f0d76868e246a2c8d7eabfb7d86683ddf88033
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2025-02-20 19:58:37 +0300

    final

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'charity_fund'
-    database_url: str = 'sqlite+aiosqlite'
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
 
     class Config:
